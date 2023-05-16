3548c4641fd32e3f853412d5b85abbe814d38833
yz-salas <115598357+yz-salas@users.noreply.github.com>
2023-05-15 22:42:26 -0400

    aguacates

--- src/index.js
+++ src/index.js
@@ -3,4 +3,49 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseUrl = "https://platzi-avo.vercel.app";
+const appNode = document.querySelector("#App");
+
+const formatPrice = (price) => {
+  const newPRICE = new window.Intl.NumberFormat("es", {
+    style: "currency", 
+    currency: "dom"
+  }).format(price);
+  return newPRICE;
+};
+
+window
+  .fetch(`${baseUrl}/api/avo`)
+  .then((respuesta) => respuesta.json())
+  .then((responseJson) => {
+    responseJson.data.forEach((item) => {
+      const todosLosItems = [];
+
+      const img = document.createElement("img");
+      img.src = `${baseUrl}${item.image}`;
+      img.className = "img";
+
+      const titulo = document.createElement("h2");
+      titulo.textContent = item.name;
+      titulo.className = "title";
+
+      const precio = document.createElement("p");
+      precio.className = "price";
+      precio.textContent = formatPrice(item.price);
+
+      const Info = document.createElement("div");
+      Info.append(titulo, precio);
+      Info.id = "info";
+
+      const contenedor = document.createElement("div");
+
+      const contIMG = document.createElement("div") 
+      contIMG.append(img)
+      contIMG.id = "contIMG"
+      contenedor.append(contIMG, Info);
+      contenedor.className = "app";
+
+      todosLosItems.push(contenedor);
+      appNode.append(...todosLosItems);
+    });
+  });
